4dcfda20f2bc215e633111830f323cdd5829a85c
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2024-03-05 14:08:41 +0530

    created folder

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { CssBaseline, Grid} from '@material-ui/core'    
+import Headers from './components/Headers/Headers'
+import List from './components/List/List'
+import Map from './components/Maps/Map'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
+
+
+
+const App = () =>{
+    return (
+        <>
+        <CssBaseline/>
+        <Headers/>
+        <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+dfghdegdfreg
+        </Grid>
+        </Grid>
+        </>
+    )
+}
+
+export default App;
